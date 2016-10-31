2547306d5caf432c2f358c3f9977634f5808548f
Hubert <hubert.jakubiak@gmail.com>
2016-10-31 23:55:34 +0100

    CRUD for boards, lists and cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,36 +1,56 @@
 class CardsController < ApplicationController
-    before_action :set_card, only: [:show]
-    def new
-        @list = List.find(params[:list_id])
-        @card = Card.new
+  before_action :set_card, only: [:show, :destroy, :edit, :update]
+  def new
+    @list = List.find(params[:list_id])
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @list = List.find(params[:list_id])
+    @card.list = @list
+    respond_to do |format|
+      if @card.save
+        format.html { redirect_to root_path, notice: 'Card was successfully created.' }
+        else
+        format.html { render :new }
+      end
     end
+  end
     
-    def create
-        @card = Card.new(card_params)
-        @list = List.find(params[:list_id])
-        @card.list = @list
-        respond_to do |format|
-            if @card.save
-            format.html { redirect_to root_path, notice: 'Card was successfully created.' }
-            format.json { render :show, status: :created, location: @list }
-            else
-            format.html { render :new }
-            format.json { render json: @card.errors, status: :unprocessable_entity }
-            end
-        end
+  def show
+        
+  end
+
+  def edit
+    @list = List.find(params[:list_id])
+  end
+
+  def update
+    respond_to do |format|
+      if @card.update(card_params)
+        format.html { redirect_to root_path, notice: 'Card was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
     end
-      
-    def show
-          
+  end
+
+  def destroy
+    @card.destroy
+      respond_to do |format|
+      format.html { redirect_to root_path, notice: 'Card was successfully destroyed.' }
     end
-      
-    private
-      
-    def card_params
-        params.require(:card).permit(:body)
-    end
+  end
     
-    def set_card
-       @card = Card.find(params[:card_id]) 
-    end
+  private
+    
+  def card_params
+    params.require(:card).permit(:body)
+  end
+  
+  def set_card
+    @card = Card.find(params[:id]) 
+  end
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,38 +1,57 @@
 class ListsController < ApplicationController
     
-    before_action :set_list, only: [:show]
-    def new
-        @board = Board.find(params[:board_id])
-        @list = List.new
+  before_action :set_list, only: [:show, :destroy, :edit, :update]
+  def new
+    @board = Board.find(params[:board_id])
+    @list = List.new
+  end
+  
+  def create
+    @list = List.new(list_params)
+    @board = Board.find(params[:board_id])
+    @list.board = @board
+    respond_to do |format|
+      if @list.save
+        format.html { redirect_to root_path, notice: 'List was successfully created.' }
+      else
+        format.html { render :new }
+      end
     end
+  end
+
+  def update
+  respond_to do |format|
+    if @list.update(list_params)
+      format.html { redirect_to root_path, notice: 'List was successfully updated.' }
+    else
+      format.html { render :edit }
+    end
+  end
+end
     
-    def create
-        @list = List.new(list_params)
-        @board = Board.find(params[:board_id])
-        @list.board = @board
-        respond_to do |format|
-            if @list.save
-            format.html { redirect_to @board, notice: 'List was successfully created.' }
-            format.json { render :show, status: :created, location: @board }
-            else
-            format.html { render :new }
-            format.json { render json: @board.errors, status: :unprocessable_entity }
-            end
-        end
+  def show
+        
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    @list.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'List was successfully destroyed.' }
     end
-      
-    def show
-          
-    end
-      
-    private
-      
-    def list_params
-        params.require(:list).permit(:title)
-    end
+  end
     
-    def set_list
-       @list = List.find(params[:id]) 
-    end
+  private
+    
+  def list_params
+    params.require(:list).permit(:title)
+  end
+
+  def set_list
+    @list = List.find(params[:id]) 
+  end
     
 end
