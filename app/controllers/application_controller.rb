1a953a911b9c6d785f8cf79fa40514931e43f400
Hubert Jakubiak <hubert.jakubiak@gmail.com>
2016-11-06 10:40:56 +0100

    Basic user authentication (#8)
    * Basic user authentication
    
    * Made changes after review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  def authenticate
+    redirect_to login_path unless current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :boards, only: [:index, :new, :create, :edit, :update, :destroy]
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
   root 'boards#index'
 end
